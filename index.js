660fe317b4ee6b9bc80707ee02ccb63b7a59727b
Rajibbepary <rajib635356@gmail.com>
2025-12-18 11:04:08 +0600

    weather app done

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 
 const apikey = '5097f6c26db8b0f17571cc60961a4b9d';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Dhaka';
 
 async function checkWeather(){
     const city = 'Dhaka';
     const response = await fetch(`${apiUrl}&q=${city}&appid=${apikey}`);
     const data = await response.json();
     console.log(data);
+    document.querySelector('.city').innerHTML= data.name;
+    document.querySelector('.temp').innerHTML= data.main.temp + "°c";
+    document.querySelector('.humidity').innerHTML= data.main.humidity + "%";
+    document.querySelector('.wind').innerHTML= data.wind.speed + "km/h";
+
 }
 
 checkWeather();
